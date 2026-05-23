414bb6620648ca017180ad2bd3ae5db9df78e363
lunecrescent69-cmyk <lunecrescent69@gmail.com>
2026-05-23 16:50:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const { chromium } = require('playwright');
 
 (async () => {
   const browser = await chromium.launch({
-    headless: true
+    headless: false
   });
 
-  const page = await browser.newPage();
+  const context = await browser.newContext({
+    storageState: 'auth.json'
+  });
+
+  const page = await context.newPage();
+
+  console.log('Opening Suno...');
 
   await page.goto('https://suno.com');
 
-  console.log('Suno opened');
+  console.log('Waiting for login...');
+
+  await page.waitForTimeout(60000);
+
+  await context.storageState({
+    path: 'auth.json'
+  });
+
+  console.log('Session saved');
 
   await browser.close();
 })();
